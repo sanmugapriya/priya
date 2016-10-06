71bfa5f78ddcf7af9c45eebcae8623e5be5bc7dc
sanmugapriya <sanmugapriya1401@gmail.com>
2016-10-06 10:40:38 +0530

    Add files via upload

---
+++ Numberchecker.C
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+int main(void) 
+{
+
+int n;
+
+scanf("%d",&n);
+
+if(n>0)
+
+printf("The number is positive");
+
+else if(n<0)
+
+printf("The number is negative");
+
+else
+
+printf("The number is zero");
+	
+return 0;
+
+}
+
