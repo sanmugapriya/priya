ea347bdb452477603b34d4234a2a43eaa242722e
sanmugapriya <sanmugapriya1401@gmail.com>
2017-02-19 09:25:14 +0530

    Update largest.c

--- largest.c
+++ largest.c
@@ -4,11 +4,11 @@ int main() {
 int a,b,c;
 printf("enter a b c value");
 scanf("%d%d%d",&a,&b,&c);
-if(a>b&&a>c)
+if(a>=b&&a>=c)
 printf("\n%d is largest number",a);
-if(b>a&&b>c)
+if(b>=a&&b>=c)
 printf("\n%d is largest number",b);
-if(c>a&&c>b)
+if(c>=a&&c>=b)
 printf("\n%dis largest number",c);
 	return 0;
 }
