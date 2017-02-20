bd5ce8d25e451d84c309a22e5b31fd235f0653e3
sanmugapriya <sanmugapriya1401@gmail.com>
2017-02-20 16:46:27 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	int low,high,i;
+	printf("enter the interval");
+	scanf("%d%d",&low,&high);
+	    for(i=low;i<=high;i++)
+	    
+	       { if(i%2!=0)
+	       
+printf("%d",i);
+}
+	return 0;
+}
