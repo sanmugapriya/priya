fecdc3ef169f3813ef08980db6867e8546107880
sanmugapriya <sanmugapriya1401@gmail.com>
2017-03-14 17:35:30 +0530

    Create table.java

---
+++ table.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n=2;
+		for(int i=1;i<=10;i++)
+		{
+		    int m=n*i;
+		    System.out.println(n+"*"+i+"="+m);
+		}
+	}
+}
